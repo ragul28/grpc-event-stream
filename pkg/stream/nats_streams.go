6a7187144f7fa2e254e839b3ec461fee7659af03
ragul28 <ragulh28@gmail.com>
2022-11-06 23:44:36 +0530

    Add: create stream func

--- pkg/stream/nats_streams.go
+++ pkg/stream/nats_streams.go
@@ -10,5 +10,19 @@ func JetStreamInit(StreamName string) (nats.JetStreamContext, error) {
 }
 
 func CreateStream(jetStream nats.JetStreamContext, StreamName string) error {
+	stream, err := jetStream.StreamInfo(StreamName)
+
+	// stream not found, create it
+	if stream == nil {
+		log.Printf("Creating stream: %s\n", StreamName)
+
+		_, err = jetStream.AddStream(&nats.StreamConfig{
+			Name:     StreamName,
+			Subjects: []string{"main"},
+		})
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
