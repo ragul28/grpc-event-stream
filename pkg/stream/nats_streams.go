c8a9764fff97cedee12255421e98eb108266a9b3
ragul28 <ragulh28@gmail.com>
2022-11-06 23:43:16 +0530

    nats: add func def

--- pkg/stream/nats_streams.go
+++ pkg/stream/nats_streams.go
@@ -1,1 +1,14 @@
 package stream
+
+import (
+	"github.com/nats-io/nats.go"
+)
+
+func JetStreamInit(StreamName string) (nats.JetStreamContext, error) {
+
+	return nil, nil
+}
+
+func CreateStream(jetStream nats.JetStreamContext, StreamName string) error {
+	return nil
+}
