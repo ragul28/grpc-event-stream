3467a158c4844c1b484214b19d347e40b079ba05
ragul28 <ragulh28@gmail.com>
2022-11-07 16:22:25 +0530

    Order create enable jetstream msg publish

--- cmd/order/main.go
+++ cmd/order/main.go
@@ -2,31 +2,42 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net"
 
+	"github.com/nats-io/nats.go"
 	pb "github.com/ragul28/grpc-event-stream/event"
 	psql "github.com/ragul28/grpc-event-stream/pkg/db"
 	"github.com/ragul28/grpc-event-stream/pkg/repository"
+	"github.com/ragul28/grpc-event-stream/pkg/stream"
 	"google.golang.org/grpc"
 )
 
 const (
-	port = ":50050"
+	port       = ":50050"
+	streamName = "ORDERS"
 )
 
 type server struct {
 	pb.UnimplementedEventServer
 	repo repository.Repository
+	nats nats.JetStreamContext
 }
 
 // CreateEvent creates a new Event
 func (s *server) CreateEvent(ctx context.Context, in *pb.EventRequest) (*pb.EventResponse, error) {
+	// Store the order on DB
 	res, err := s.repo.CreateOrder(in)
 	if err != nil {
 		return nil, err
 	}
 
+	values := map[string]string{"id": in.Id, "name": in.Name}
+	jsonValue, _ := json.Marshal(values)
+
+	// Publish order on nats jetstream
+	s.nats.Publish(streamName, jsonValue)
 	return res, nil
 }
 
@@ -51,6 +62,12 @@ func main() {
 	log.Println("DB Successfully connected!")
 	repository := &repository.OrderRepository{DB: db}
 
+	js, err := stream.JetStreamInit(streamName)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -60,6 +77,7 @@ func main() {
 
 	server := &server{
 		repo: repository,
+		nats: js,
 	}
 	pb.RegisterEventServer(s, server)
 
