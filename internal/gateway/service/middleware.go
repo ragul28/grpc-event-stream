0b2785174b23c7a42de49aafad1f41e9ebd96967
ragul28 <ragulh28@gmail.com>
2022-11-17 22:41:26 +0530

    init gateway service

---
+++ internal/gateway/service/middleware.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type Middleware struct{}
+
+// General noauth middleware
+func (m *Middleware) Middleware(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+		log.Println(r.Method, r.RequestURI, time.Since(startTime))
+	})
+}
+
+// allowCORS from any origin. TODO disable in prod
+func (m *Middleware) AllowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+// preflightHandler adds headers and CROS from ACAM
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+	log.Println("preflight request for", r.URL.Path)
+}
---
+++ internal/gateway/service/repository.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/net/context"
+
+	pb "github.com/ragul28/grpc-event-stream/event"
+	"github.com/ragul28/grpc-event-stream/internal/model"
+)
+
+type Repository interface {
+	CreateOrder(client pb.EventClient, Event *pb.EventRequest) error
+	GetOrder(client pb.EventClient, filter *pb.GetEventFilter) (*model.OrderEvent, error)
+}
+
+type SrvRepository struct{}
+
+// createEvent calls the RPC method CreateEvent of EventServer
+func (s *SrvRepository) CreateOrder(client pb.EventClient, Event *pb.EventRequest) error {
+	resp, err := client.CreateEvent(context.Background(), Event)
+	if err != nil {
+		return fmt.Errorf("Could not create Event: %v", err)
+	}
+	if resp.Success {
+		log.Printf("Event added id: %s", resp.Id)
+	}
+
+	return nil
+}
+
+// getEvents calls the RPC method GetEvents of EventServer
+func (s *SrvRepository) GetOrder(client pb.EventClient, filter *pb.GetEventFilter) (*model.OrderEvent, error) {
+	// calling the streaming API
+	resp, err := client.GetEvent(context.Background(), filter)
+	if err != nil {
+		return nil, fmt.Errorf("Error on get Event: %v", err)
+	}
+	log.Printf("Get Event id %s: %s", filter.Id, resp.Name)
+
+	return &model.OrderEvent{Id: resp.Id, Name: resp.Name}, nil
+}
